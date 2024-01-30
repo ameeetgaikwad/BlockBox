b28370ad18680f02bcb406a626d76d3c02ea9073
Amit Gaikwad <amitmanojgaikwad@gmail.com>
2024-01-30 19:38:50 +0530

    chore: page titles added

--- src/pages/creator/[_id].tsx
+++ src/pages/creator/[_id].tsx
@@ -1,6 +1,7 @@
 import Layout from "@/components/Layout/Layout";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import axios from "axios";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
@@ -45,51 +46,58 @@ function SubscriptionDetail() {
     }
   }, [isConnected, account]);
   return (
-    <Layout>
-      {display ? (
-        <div className="flex flex-col min-h-screen">
-          <main className="flex-1 py-8 px-6 ">
-            <div className="max-w-5xl mx-auto space-y-12">
-              <div className="grid gap-8 lg:grid-cols-2">
-                {subscriptions?.image ? (
-                  <img
-                    src={subscriptions?.image}
-                    alt="nft"
-                    width={300}
-                    height={200}
-                  />
-                ) : (
-                  <img
-                    src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-                    alt="nft"
-                    width={300}
-                    height={400}
-                  />
-                )}
-                <div className="space-y-4">
-                  <h1 className="text-3xl font-bold">{subscriptions?.title}</h1>
-                  <div className="flex flex-row justify-between">
-                    <span className="badge badge-outline badge-accent">
-                      {subscriptions?.symbol}
-                    </span>
-                    <span className="badge badge-neutral">
-                      {subscriptions?.price}ether
-                    </span>
+    <>
+      <Head>
+        <title>Membership details</title>
+      </Head>
+      <Layout>
+        {display ? (
+          <div className="flex flex-col min-h-screen">
+            <main className="flex-1 py-8 px-6 ">
+              <div className="max-w-5xl mx-auto space-y-12">
+                <div className="grid gap-8 lg:grid-cols-2">
+                  {subscriptions?.image ? (
+                    <img
+                      src={subscriptions?.image}
+                      alt="nft"
+                      width={300}
+                      height={200}
+                    />
+                  ) : (
+                    <img
+                      src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                      alt="nft"
+                      width={300}
+                      height={400}
+                    />
+                  )}
+                  <div className="space-y-4">
+                    <h1 className="text-3xl font-bold">
+                      {subscriptions?.title}
+                    </h1>
+                    <div className="flex flex-row justify-between">
+                      <span className="badge badge-outline badge-accent">
+                        {subscriptions?.symbol}
+                      </span>
+                      <span className="badge badge-neutral">
+                        {subscriptions?.price}ether
+                      </span>
+                    </div>
+                    <p className="text-gray-500 dark:text-gray-400">
+                      {subscriptions?.benifits.map((benifit, _id) => {
+                        return <p key={_id}>{benifit}</p>;
+                      })}
+                    </p>
                   </div>
-                  <p className="text-gray-500 dark:text-gray-400">
-                    {subscriptions?.benifits.map((benifit, _id) => {
-                      return <p key={_id}>{benifit}</p>;
-                    })}
-                  </p>
                 </div>
               </div>
-            </div>
-          </main>
-        </div>
-      ) : (
-        <ConnectButton />
-      )}
-    </Layout>
+            </main>
+          </div>
+        ) : (
+          <ConnectButton />
+        )}
+      </Layout>
+    </>
   );
 }
 
--- src/pages/creator/index.tsx
+++ src/pages/creator/index.tsx
@@ -1,8 +1,12 @@
 import MyCreator from "@/views/Creator/MyCreator";
+import Head from "next/head";
 
 function Creator() {
   return (
     <>
+      <Head>
+        <title>Create Memberships</title>
+      </Head>
       <MyCreator />
     </>
   );
